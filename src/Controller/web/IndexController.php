4e0a1cf705f160d3fd678dc4e1dad3e93e081434
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-05-06 15:16:28 +0200

    Update

--- src/Controller/web/IndexController.php
+++ src/Controller/web/IndexController.php
@@ -38,13 +38,9 @@ final class IndexController extends AbstractController
 
 
     public function __construct(
-
         RouterInterface $router
-
     ) {
-
         $this->router = $router;
-
     }
 
     /**
@@ -52,9 +48,62 @@ final class IndexController extends AbstractController
      */
     public function index(Request $request): Response
     {
-
         return $this->render('index.html.twig');
     }
 
+    /**
+     * @Route("/feature", name="feature", methods={"GET"})
+     */
+    public function feature(Request $request): Response
+    {
+        return $this->render('feature.html.twig');
+    }
 
+    /**
+     * @Route("/services", name="services", methods={"GET"})
+     */
+    public function services(Request $request): Response
+    {
+        return $this->render('services.html.twig');
+    }
+
+    /**
+     * @Route("/pricing", name="pricing", methods={"GET"})
+     */
+    public function pricing(Request $request): Response
+    {
+        return $this->render('pricing.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact", methods={"GET"})
+     */
+    public function contact(Request $request): Response
+    {
+        return $this->render('contact.html.twig');
+    }
+
+    /**
+     * @Route("/blog", name="blog", methods={"GET"})
+     */
+    public function blog(Request $request): Response
+    {
+        return $this->render('blog.html.twig');
+    }
+
+    /**
+     * @Route("/single-blog", name="single-blog", methods={"GET"})
+     */
+    public function sblog(Request $request): Response
+    {
+        return $this->render('single-blog.html.twig');
+    }
+
+    /**
+     * @Route("/elements", name="elements", methods={"GET"})
+     */
+    public function elements(Request $request): Response
+    {
+        return $this->render('elements.html.twig');
+    }
 }
