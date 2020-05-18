9283c649f2f2f1c58289e3ccf77b423ace432782
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-05-18 10:16:17 +0200

    #13 First create of Helper section

---
+++ src/Controller/profile/HelperController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller\profile;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HelperController extends AbstractController
+{
+    /**
+     * @Route("/helper-list", name="helperlist", methods={"GET"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function start(): Response
+    {
+        return $this->render('profile/helperlist.html.twig');
+    }
+
+    /**
+     * @Route("/helper-edit", name="helper_edit", methods={"GET"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function edit(): Response
+    {
+    }
+
+    /**
+     * @Route("/helper-delete", name="helper_delete", methods={"GET"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function delete(): Response
+    {
+    }
+}
--- src/Controller/profile/StartController.php
+++ src/Controller/profile/StartController.php
@@ -2,6 +2,19 @@
 
 namespace App\Controller\profile;
 
-class StartController
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StartController extends AbstractController
 {
+    /**
+     * @Route("/start", name="start", methods={"GET"})
+     * @IsGranted("ROLE_USER")
+     */
+    public function start(): Response
+    {
+        return $this->render('profile/start.html.twig');
+    }
 }
