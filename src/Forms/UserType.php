14a849504cc27f25c8e95aa12e0f2db6b45658cf
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-05-28 08:19:00 +0200

    #14 Added update contact area

--- src/Forms/UserType.php
+++ src/Forms/UserType.php
@@ -3,7 +3,6 @@
 namespace App\Forms;
 
 use App\Entity\User;
-use Doctrine\DBAL\Types\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
