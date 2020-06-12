51073835e47361d70e4eedc75097f6979e184997
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-06-12 14:26:33 +0200

    #17 Added login feature from API

--- src/Controller/RestApi/APIController.php
+++ src/Controller/RestApi/APIController.php
@@ -5,11 +5,14 @@ namespace App\Controller\RestApi;
 use App\Entity\Helper;
 use App\Entity\User;
 use App\Services\GeoCoderApi;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class APIController extends AbstractController
 {
@@ -18,10 +21,17 @@ class APIController extends AbstractController
      */
     private $mailer;
 
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $userPasswordEncoder;
+
     public function __construct(
-        MailerInterface $mailer
+        MailerInterface $mailer,
+        UserPasswordEncoderInterface $passwordEncoder
     ) {
         $this->mailer = $mailer;
+        $this->userPasswordEncoder = $passwordEncoder;
     }
 
     /**
@@ -100,31 +110,64 @@ class APIController extends AbstractController
 
         return $this->render('api/sucess.html.twig');
     }
-    
+
     /**
      * @Route("/RegisterDevice", name="RegisterDevice", methods={"GET"})
      */
     public function RegisterDevice(Request $request): Response
     {
-        
         $userId = $request->get('userID');
         if (empty($userId)) {
             return $this->render('api/fail.html.twig');
         }
         try {
-             $user = $this->getDoctrine()
+            $user = $this->getDoctrine()
             ->getRepository(User::class)
             ->find($userId);
         } catch (Exception $e) {
-             return $this->render('api/fail.html.twig');
+            return $this->render('api/fail.html.twig');
         }
-       
+
         if (!$user) {
             return $this->render('api/fail.html.twig');
         } elseif (0 != strcmp($user->getJWT(), $request->get('jwt'))) {
             return $this->render('api/fail.html.twig');
         }
+
         return $this->render('api/sucess.html.twig');
     }
-    
+
+    /**
+     * @Route("/login", name="login", methods={"GET"})
+     */
+    public function login(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $email = $request->get('email');
+        $plainPassword = $request->get('password');
+        if (empty($email) and !empty($plainPassword)) {
+            $response->setData(['sucess' => false]);
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($email);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+        }
+
+        $encodedPassword = $this->userPasswordEncoder->encodePassword($user, $plainPassword);
+
+        if (0 != strcmp($user->getPassword(), $encodedPassword)) {
+            $response->setData(['sucess' => false]);
+        } else {
+            $response->setData(['sucess' => true, 'userID' => $user->getId(), 'jwt' => $user->getJwt()]);
+        }
+
+        return $response;
+    }
 }
