81823cd48fc397acedb199a3ce9ffd87caffee80
Lukas Walkenbach <lukas.walkenbach@gmail.com>
2020-08-06 17:01:09 +0200

    #49 Added all CRUD functions to REST API

--- src/Controller/RestApi/APIController.php
+++ src/Controller/RestApi/APIController.php
@@ -4,6 +4,7 @@ namespace App\Controller\RestApi;
 
 use App\Entity\Device;
 use App\Entity\Helper;
+use App\Entity\Medicals;
 use App\Entity\User;
 use App\Services\GeoCoderApi;
 use Doctrine\ORM\EntityManagerInterface;
@@ -253,6 +254,554 @@ class APIController extends AbstractController
 
             return $response;
         }
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/addHelper", name="add_helper", methods={"GET"})
+     */
+    public function addHelper(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+
+        $helperFirstname = $data['firstname'];
+        $helperLastname = $data['lastname'];
+        $helperEmail = $data['email'];
+
+        if (empty($userId) or empty($jwt) or empty($helperFirstname) or empty($helperLastname) or empty($helperEmail)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $request->get('jwt'))) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $helper = new Helper();
+
+            $helper->setEmail($helperEmail);
+            $helper->setFirstname($helperFirstname);
+            $helper->setLastname($helperLastname);
+
+            $helper->setUserid($userId);
+
+            $this->entityManager->persist($helper);
+
+            $this->entityManager->flush();
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/addMedical", name="add_medical", methods={"GET"})
+     */
+    public function addMedical(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+
+        $medicalName = $data['name'];
+        $medicalDose = $data['dose'];
+        $medicalLink = $data['link'];
+
+        if (empty($userId) or empty($jwt) or empty($medicalName) or empty($medicalDose) or empty($medicalLink)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $medical = new Medicals();
+
+            $medical->setName($medicalName);
+            $medical->setDosis($medicalDose);
+            $medical->setLink($medicalLink);
+
+            $medical->setUser($user);
+
+            $this->entityManager->persist($medical);
+
+            $this->entityManager->flush();
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/getAllHelper", name="get_all_Helper", methods={"GET"})
+     */
+    public function getAllHelper(Request $request): JSONResponse
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+
+        if (empty($userId) or empty($jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        $helpers = $user->getHelpers();
+
+        $data = [];
+        foreach ($helpers as $helper) {
+            $data[] = $helper->toArray();
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getAllMedical", name="get_all_Medical", methods={"GET"})
+     */
+    public function getAllMedical(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+
+        if (empty($userId) or empty($jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        $medicals = $user->getMedicals();
+
+        $data = [];
+        foreach ($medicals as $medical) {
+            $data[] = $medical->toArray();
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getMedical", name="get_Medical", methods={"GET"})
+     */
+    public function getMedical(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $medicalId = $data['medicalID'];
+        if (empty($userId) or empty($jwt) or empty($medicalId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        $medicals = $user->getMedicals();
+
+        $data = [];
+        foreach ($medicals as $medical) {
+            if (0 != strcmp($medical->getId(), $medicalId)) {
+            }
+            $data[] = $medical->toArray();
+        }
+
+        if (empty($data)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/getHelper", name="get_Medical", methods={"GET"})
+     */
+    public function getHelper(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $helperId = $data['helperID'];
+        if (empty($userId) or empty($jwt) or empty($medicalId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        $helpers = $user->getHelpers();
+
+        $data = [];
+        foreach ($helpers as $helper) {
+            if (0 != strcmp($helper->getId(), $helperId)) {
+            }
+            $data[] = $helper->toArray();
+        }
+
+        if (empty($data)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        return new JsonResponse($data, Response::HTTP_OK);
+    }
+
+    /**
+     * @Route("/updateMedical", name="update_Medical", methods={"GET"})
+     */
+    public function updateMedical(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $medicalId = $data['medicalID'];
+
+        if (empty($userId) or empty($jwt) or empty($medicalId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        try {
+            $medical = $this->getDoctrine()
+                ->getRepository(Medicals::class)
+                ->find($medicalId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        empty($data['medicalName']) ? true : $medical->setName($data['medicalName']);
+        empty($data['medicalLink']) ? true : $medical->setLink($data['medicalLink']);
+        empty($data['medicalDose']) ? true : $medical->setDosis($data['medicalDose']);
+
+        $this->entityManager->persist($medical);
+        $this->entityManager->flush();
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/updateHelper", name="update_Helper", methods={"GET"})
+     */
+    public function updateHelper(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $helperId = $data['helperID'];
+
+        if (empty($userId) or empty($jwt) or empty($helperId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        try {
+            $helper = $this->getDoctrine()
+                ->getRepository(Helper::class)
+                ->find($helperId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        empty($data['firstname']) ? true : $helper->setFirstname($data['firstname']);
+        empty($data['lastname']) ? true : $helper->setLastname($data['lastname']);
+        empty($data['email']) ? true : $helper->setEmail($data['email']);
+
+        $this->entityManager->persist($helper);
+        $this->entityManager->flush();
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/deleteMedical", name="delete_Medical", methods={"GET"})
+     */
+    public function deleteMedical(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $medicalId = $data['medicalID'];
+
+        if (empty($userId) or empty($jwt) or empty($medicalId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        try {
+            $medical = $this->getDoctrine()
+                ->getRepository(Helper::class)
+                ->find($medicalId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        $this->entityManager->remove($medical);
+        $this->entityManager->flush();
+
+        $response->setData(['sucess' => true]);
+
+        return $response;
+    }
+
+    /**
+     * @Route("/deleteHelper", name="delete_Helper", methods={"GET"})
+     */
+    public function deleteHelper(Request $request): Response
+    {
+        $response = new JsonResponse();
+        $data = json_decode($request->getContent(), true);
+
+        $userId = $data['userID'];
+        $jwt = $data['jwt'];
+        $helperId = $data['helperID'];
+
+        if (empty($userId) or empty($jwt) or empty($helperId)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+        try {
+            $user = $this->getDoctrine()
+                ->getRepository(User::class)
+                ->find($userId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        if (!$user) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        } elseif (0 != strcmp($user->getJWT(), $jwt)) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        try {
+            $helper = $this->getDoctrine()
+                ->getRepository(Helper::class)
+                ->find($helperId);
+        } catch (Exception $e) {
+            $response->setData(['sucess' => false]);
+
+            return $response;
+        }
+
+        $this->entityManager->remove($helper);
+        $this->entityManager->flush();
 
         $response->setData(['sucess' => true]);
 
